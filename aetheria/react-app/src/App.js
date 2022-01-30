6ba65ea86e0e385ef1513f307f41caae9729b7ec
NoahNim <nmedoff123@gmail.com>
2022-01-29 21:25:14 -0800

    add auth code to front end

--- aetheria/react-app/src/App.js
+++ aetheria/react-app/src/App.js
@@ -1,13 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import LoginFormPage from "./components/auth/LoginFormPage";
+import { authenticate } from "./store/session";
 
 function App() {
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      await dispatch(authenticate());
+      setLoaded(true);
+    })();
+  }, [dispatch]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <BrowserRouter>
-
+      <Switch>
+        <Route path="/login" exact={true}>
+          <LoginFormPage />
+        </Route>
+        </Switch>
     </BrowserRouter>
   );
 }
