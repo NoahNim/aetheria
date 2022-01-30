f169f2483ea89f78d3a49b4029d9ecaff9c627aa
NoahNim <nmedoff123@gmail.com>
2022-01-29 17:23:11 -0800

    made logon form and user model

--- aetheria/app/__init__.py
+++ aetheria/app/__init__.py
@@ -5,9 +5,16 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 
+from .models import db, User
+
 from .config import Config
 
 app = Flask(__name__)
+
+# Tell flask about our seed commands
+# app.cli.add_command(seed_commands)
+
+app.config.from_object(Config)
 
 @app.route('/')
 def hello_world():
