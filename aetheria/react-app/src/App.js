a6628209dd0e0ac842757b95d09b87750c7d5371
NoahNim <nmedoff123@gmail.com>
2022-02-01 18:21:01 -0800

    delete buggy code going to rewite and recode a lot

--- aetheria/react-app/src/App.js
+++ aetheria/react-app/src/App.js
@@ -1,41 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Provider, useDispatch, useSelector } from "react-redux";
-import LoginFormPage from "./components/auth/LoginFormPage";
-import ContentWrap from "./components/ContentWrap";
-import { authenticate } from "./store/session";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
-  const [loaded, setLoaded] = useState(false);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    (async () => {
-      await dispatch(authenticate());
-      setLoaded(true);
-    })();
-  }, [dispatch]);
-
-  if (!loaded) {
-    return null;
-  }
 
   return (
     <>
-      {loaded && (
-        < BrowserRouter >
-          <Provider store={loaded}>
-          <ContentWrap>
-            <Switch>
-              <Route path="/login" exact={true}>
-                <LoginFormPage />
-              </Route>
-            </Switch>
-            </ContentWrap>
-          </Provider>
-        </BrowserRouter >
-      )
-      }
+     fffff
     </>
   );
 }
--- aetheria/react-app/src/components/ContentWrap/ContentWrap.js
+++
@@ -1,7 +0,0 @@
-import styles from "./ContentWrap.module.css";
-
-const ContentWrap = ({ children }) => {
-	return <div className={styles.content_wrap}>{children}</div>;
-};
-
-export default ContentWrap;
